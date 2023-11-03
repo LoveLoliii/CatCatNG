e5be9be7b7245c12376ffbed0519e3c75941e6a8
loveloliii <loli@loli.monster>
2023-11-03 16:00:47 +0800

    ui。

---
+++ SettingForm.Designer.cs
@@ -0,0 +1,79 @@
+﻿namespace WinFormsApp2
+{
+    partial class SettingForm
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            startDanmuFormBtn = new Button();
+            pictureBox1 = new PictureBox();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
+            SuspendLayout();
+            // 
+            // startDanmuFormBtn
+            // 
+            startDanmuFormBtn.FlatAppearance.BorderSize = 0;
+            startDanmuFormBtn.FlatStyle = FlatStyle.Flat;
+            startDanmuFormBtn.Image = CatCatNG.Properties.Resources.play1;
+            startDanmuFormBtn.Location = new Point(39, 464);
+            startDanmuFormBtn.Margin = new Padding(72, 33, 72, 33);
+            startDanmuFormBtn.Name = "startDanmuFormBtn";
+            startDanmuFormBtn.Size = new Size(46, 46);
+            startDanmuFormBtn.TabIndex = 0;
+            startDanmuFormBtn.UseVisualStyleBackColor = true;
+            startDanmuFormBtn.Click += button1_Click;
+            // 
+            // pictureBox1
+            // 
+            pictureBox1.Location = new Point(15, 20);
+            pictureBox1.Margin = new Padding(72, 33, 72, 33);
+            pictureBox1.Name = "pictureBox1";
+            pictureBox1.Size = new Size(145, 144);
+            pictureBox1.TabIndex = 1;
+            pictureBox1.TabStop = false;
+            pictureBox1.Click += pictureBox1_Click;
+            // 
+            // SettingForm
+            // 
+            AutoScaleDimensions = new SizeF(144F, 144F);
+            AutoScaleMode = AutoScaleMode.Dpi;
+            ClientSize = new Size(917, 773);
+            Controls.Add(pictureBox1);
+            Controls.Add(startDanmuFormBtn);
+            Margin = new Padding(72, 33, 72, 33);
+            Name = "SettingForm";
+            Text = "Form1";
+            Load += Form1_Load;
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private Button startDanmuFormBtn;
+        private PictureBox pictureBox1;
+    }
+}
---
+++ SettingForm.cs
@@ -0,0 +1,58 @@
+using System.Net;
+using WebP.Net;
+
+namespace WinFormsApp2
+{
+    public partial class SettingForm : Form
+    {
+        public SettingForm()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            String webpImageUrl = "https://i0.hdslb.com/bfs/new_dyn/750c0c53bbee5e1d4f151b3ac7236bd21999280.png@180w_180h_1e_1c.webp";
+
+            using (WebClient webClient = new WebClient())
+            {
+                byte[] webpData = webClient.DownloadData(webpImageUrl);
+
+                using (MemoryStream stream = new MemoryStream(webpData))
+                {
+                    // 使用 LibwebpSharp 来解码 WebP 图像
+                    
+
+                    // 将 WebP 图像转换为 Bitmap
+                    Bitmap bitmap = WebPDecoder.Decode(webpData);
+
+                    int buttonWidth = 145;
+                    int buttonHeight = 145;
+                    Image scaledImage = new Bitmap(buttonWidth, buttonHeight);
+
+                    using (Graphics graphics = Graphics.FromImage(scaledImage))
+                    {
+                        graphics.InterpolationMode = System.Drawing.Drawing2D.InterpolationMode.HighQualityBicubic;
+                        graphics.DrawImage(bitmap, new Rectangle(0, 0, buttonWidth, buttonHeight), new Rectangle(0, 0, bitmap.Width, bitmap.Height), GraphicsUnit.Pixel);
+                    }
+
+                    // 设置 PictureBox 的图像
+                    pictureBox1.Image = scaledImage;
+                }
+            }
+            Console.WriteLine("load from");
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            DanmuForm newForm = new();
+
+            newForm.Show();
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}

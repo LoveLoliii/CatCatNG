263546386c1aac0ef20e34b647ec175700d071a3
loveloliii <loli@loli.monster>
2023-11-07 17:58:57 +0800

    login

---
+++ vo/BilibiliHttpResponse.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CatCatNG.vo
+{
+    /*
+     * struct like this "
+     * {
+     *       "code": 0,
+     *      "status": true,
+     *       "ts": 1699348050,
+     *       "data": {
+     *           "url": "https://passport.bilibili.com/qrcode/h5/login?oauthKey=ssssssssssssssss",
+     *           "oauthKey": "ssssssssssssssssssssssssssss"
+     *       }
+     *   }"
+     * 
+     * **/
+    public class BilibiliHttpResponse
+    {
+        public int Code { get; set; }
+        public bool Status { get; set; }
+        public long Ts { get; set; }
+        public BilibiliLoginResponseData? Data { get; set; }
+    }
+}
---
+++ vo/BilibiliLoginResponse.cs
@@ -0,0 +1,8 @@
+﻿namespace CatCatNG.vo
+{
+    public class BilibiliLoginResponseData
+    {
+        public string? Url { get; set; }
+        public string? OauthKey { get; set; }
+    }
+}
